50834bfe44de4b68f9292cf04ae2a02850117bef
Anthony prabakaran <77183199+Antonypraba@users.noreply.github.com>
2023-12-20 16:49:43 +0530

    Add files via upload

---
+++ js/scriptpr.js
@@ -0,0 +1,21 @@
+// let preveiwContainer = document.querySelector('.products-preview');
+// let previewBox = preveiwContainer.querySelectorAll('.preview');
+//
+// document.querySelectorAll('.products-container .product').forEach(product =>{
+//   product.onclick = () =>{
+//     let name = product.getAttribute('data-name');
+//     previewBox.forEach(preview =>{
+//       let target = preview.getAttribute('data-target');
+//       if(name == target){
+//         preview.classList.add('active');
+//       }
+//     });
+//   };
+// });
+//
+// previewBox.forEach(close =>{
+//   close.querySelector('.fa-times').onclick = () =>{
+//     close.classList.remove('active');
+//     preveiwContainer.style.display = 'none';
+//   };
+// });
